f58353ff1e69f01f3f3ee85899130ed067a640ac
den_minus <den.minus@mail.ru>
2023-01-27 14:56:03 +0300

    подсчитать сумму цифр в числе, есть вопрос

---
+++ Task2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Подсчитать сумму цифр в числе
+
+
+/*Console.WriteLine("Введите число");
+string str= Console.ReadLine();
+int lenght = str.Length; 
+int sum=0;
+    for(int i=0; i<lenght; i++)
+    {
+        
+        int value = Convert.ToInt32(str[i]);
+        
+        sum=sum+value;
+        
+        Console.WriteLine(value);
+
+    }
+Console.WriteLine(sum);
+
+*/
+// Здравствуйте. Особенностью метода Console.ReadLine() является то, 
+// что он может считать информацию с консоли только в виде строки.
+// Почему при int value = Convert.ToInt32(Console.ReadLine()); конвертация нормально проходит,  а
+// при int value = Convert.ToInt32(str[i]); конвертация проходит как то криво?
+
+Console.WriteLine("Введите число");
+int value = Convert.ToInt32(Console.ReadLine());
+int sum=0;
+while (value > 0)
+{
+    int fig = value % 10;
+    sum=sum+fig;
+    value = value / 10;
+}
+Console.WriteLine(sum);
