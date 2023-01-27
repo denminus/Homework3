9d370a2e480d2f31bc5462369bdf66abe5f22468
den_minus <den.minus@mail.ru>
2023-01-27 15:07:34 +0300

    программа вычисления произведения чисел от 1 до N

---
+++ Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+Console.WriteLine("Введите количество чисел N");
+int N = int.Parse(Console.ReadLine()!);
+ Console.WriteLine();
+int i=1;
+int sum=0;
+while ( i<=N)    
+    {
+        sum= sum+i;        
+        i++;       
+    }
+Console.WriteLine(sum);
