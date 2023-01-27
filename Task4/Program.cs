65cf13dc53868639c73488413947b3966f266b7b
den_minus <den.minus@mail.ru>
2023-01-27 22:44:06 +0300

    кубы чисел от 1 до N , заканчивающихся на четную цифру

---
+++ Task4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+Console.WriteLine("Введите количество чисел N");
+int N = int.Parse(Console.ReadLine()!);
+ Console.WriteLine();
+int i=1;
+while ( i<=N)    
+    {
+        int Cub= i*i*i;
+        if(Cub%2==0)
+        {
+            Console.Write("куб числа, заканчивающийся на четную цифру ");
+            Console.WriteLine(Cub);
+
+        }
+        
+        i++;       
+    }
