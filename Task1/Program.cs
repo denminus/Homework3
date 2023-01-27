2aa5bfa0b23d88af00cced888a108bd8dda8ac70
den_minus <den.minus@mail.ru>
2023-01-27 13:40:53 +0300

    Кубы чисел от 1 до N

---
+++ Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти кубы чисел от 1 до N
+Console.WriteLine("Введите количество чисел N");
+int N = int.Parse(Console.ReadLine()!);
+ Console.WriteLine();
+int i=1;
+while ( i<=N)    
+    {
+        int Cub= i*i*i;
+        Console.WriteLine(Cub);
+        i++;       
+    }
